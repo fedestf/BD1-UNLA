05df9ee3981b77d91970472010749d77ff7a1f05
fedestf <fede520@live.com>
2019-08-16 19:18:18 -0300

    Update CODIGO BD.sql

--- SCRIPTS/CODIGO BD.sql
+++ SCRIPTS/CODIGO BD.sql
@@ -1,2 +1,73 @@
 create schema terminalauto;
 
+CREATE TABLE IF NOT EXISTS autoparte (
+    idautoparte INT NOT NULL,
+    nombre VARCHAR(45),
+    PRIMARY KEY (idautoparte)
+);
+
+CREATE TABLE IF NOT EXISTS proveedor (
+    idproveedor INT NOT NULL,
+    nombre VARCHAR(45),
+    PRIMARY KEY (idproveedor)
+);
+
+CREATE TABLE IF NOT EXISTS fabrica (
+    idfabrica INT NOT NULL,
+    nombre VARCHAR(45),
+    PRIMARY KEY (idfabrica)
+);
+
+CREATE TABLE IF NOT EXISTS modelo (
+    idmodelo INT NOT NULL,
+    nombre VARCHAR(45),
+    precio INT NOT NULL,
+    PRIMARY KEY (idmodelo)
+);
+
+CREATE TABLE IF NOT EXISTS lineaDeMontaje (
+    idlineaDeMontaje INT NOT NULL,
+    idfabrica INT NOT NULL,
+    idmodelo INT NOT NULL,
+    PRIMARY KEY (idlineaDeMontaje),
+    FOREIGN KEY (idfabrica)
+        REFERENCES fabrica (idfabrica),
+    FOREIGN KEY (idmodelo)
+        REFERENCES modelo (idmodelo)
+);
+
+CREATE TABLE IF NOT EXISTS insumo (
+    idinsumo INT NOT NULL,
+    nombre VARCHAR(45),
+    PRIMARY KEY (idinsumo)
+);
+
+CREATE TABLE IF NOT EXISTS estacion (
+    idestacion INT NOT NULL,
+    idlineaDeMontaje INT NOT NULL,
+    idinsumo INT NOT NULL,
+    tarea VARCHAR(45),
+    PRIMARY KEY (idestacion),
+    FOREIGN KEY (idlineaDeMontaje)
+        REFERENCES lineaDeMontaje (idlineaDeMontaje),
+    FOREIGN KEY (idautoparte)
+        REFERENCES autoparte (idautoparte),
+    FOREIGN KEY (idinsumo)
+        REFERENCES insumo (idinsumo)
+);
+
+-- hasta aca hice yo , despues si no anda es culpa de sol jajajaja --
+
+
+
+
+
+
+
+
+
+
+
+
+
+
