6fc33f3f1cd4b7dabe4bd7c5a079d6be4cde0125
Sol <tekirayi@yahoo.com.ar>
2019-08-18 18:45:44 -0300

    codigo de las tablas
    falta revisar una fk de estacion

--- SCRIPTS/CODIGO BD.sql
+++ SCRIPTS/CODIGO BD.sql
@@ -1,23 +1,29 @@
+drop schema if exists terminalauto;
 create schema terminalauto;
+use terminalauto;
 
+DROP TABLE IF EXISTS autoparte;
 CREATE TABLE IF NOT EXISTS autoparte (
     idautoparte INT NOT NULL,
     nombre VARCHAR(45),
     PRIMARY KEY (idautoparte)
 );
 
+DROP TABLE IF EXISTS proveedor;
 CREATE TABLE IF NOT EXISTS proveedor (
     idproveedor INT NOT NULL,
     nombre VARCHAR(45),
     PRIMARY KEY (idproveedor)
 );
 
+DROP TABLE IF EXISTS fabrica;
 CREATE TABLE IF NOT EXISTS fabrica (
     idfabrica INT NOT NULL,
     nombre VARCHAR(45),
     PRIMARY KEY (idfabrica)
 );
 
+DROP TABLE IF EXISTS modelo;
 CREATE TABLE IF NOT EXISTS modelo (
     idmodelo INT NOT NULL,
     nombre VARCHAR(45),
@@ -25,6 +31,7 @@ CREATE TABLE IF NOT EXISTS modelo (
     PRIMARY KEY (idmodelo)
 );
 
+DROP TABLE IF EXISTS lineaDeMontaje;
 CREATE TABLE IF NOT EXISTS lineaDeMontaje (
     idlineaDeMontaje INT NOT NULL,
     idfabrica INT NOT NULL,
@@ -36,12 +43,14 @@ CREATE TABLE IF NOT EXISTS lineaDeMontaje (
         REFERENCES modelo (idmodelo)
 );
 
+DROP TABLE IF EXISTS insumo;
 CREATE TABLE IF NOT EXISTS insumo (
     idinsumo INT NOT NULL,
     nombre VARCHAR(45),
     PRIMARY KEY (idinsumo)
 );
 
+DROP TABLE IF EXISTS estacion;
 CREATE TABLE IF NOT EXISTS estacion (
     idestacion INT NOT NULL,
     idlineaDeMontaje INT NOT NULL,
@@ -56,18 +65,87 @@ CREATE TABLE IF NOT EXISTS estacion (
         REFERENCES insumo (idinsumo)
 );
 
--- hasta aca hice yo , despues si no anda es culpa de sol jajajaja --
+-- ----------------------------------------------------
+DROP TABLE IF EXISTS concesionaria;
+CREATE TABLE IF NOT EXISTS concesionaria(
+	idconcesionaria INT NOT NULL,
+	nombre VARCHAR(45) NOT NULL,
+	PRIMARY KEY (idconcesionaria)
+  ) ;
 
+DROP TABLE IF EXISTS vehiculo;
+CREATE TABLE IF NOT EXISTS vehiculo (
+	numChasis VARCHAR(20) NOT NULL,
+	idmodelo INT NOT NULL,
+	PRIMARY KEY (numChasis),
+	FOREIGN KEY (idmodelo)
+		REFERENCES modelo (idmodelo)
+    );
 
+DROP TABLE IF EXISTS pedidoAutopartes;
+CREATE TABLE IF NOT EXISTS pedidoAutopartes(
+	idpedidoAutop INT AUTO_INCREMENT,
+	idautoparte INT NOT NULL,
+	idproveedor INT NOT NULL,
+	idfabrica INT NOT NULL,
+	fecha DATE NOT NULL,
+	precio FLOAT NOT NULL,
+	cantidad INT NOT NULL,
+	PRIMARY KEY (idpedidoAutop),
+	FOREIGN KEY (idautoparte)
+		REFERENCES autoparte (idautoparte),
+	FOREIGN KEY (idproveedor)
+		REFERENCES proveedor (idproveedor),
+	FOREIGN KEY (idfabrica)
+		REFERENCES fabrica(idfabrica)
+	);
 
+DROP TABLE IF EXISTS venta;
+CREATE TABLE IF NOT EXISTS venta (
+	idventa INT AUTO_INCREMENT,
+	idconcesionaria INT NOT NULL,
+	idmodelo INT NOT NULL,
+	idfabrica INT NOT NULL,
+	cantidad INT NOT NULL,
+	fecha DATE NOT NULL,
+	PRIMARY KEY (idventa),
+	FOREIGN KEY (idconcesionaria)
+		REFERENCES concesionaria (idconcesionaria),
+	FOREIGN KEY (idfabrica)
+		REFERENCES fabrica (idfabrica),
+	FOREIGN KEY (idmodelo)
+		REFERENCES modelo (idmodelo)
+);
 
+DROP TABLE IF EXISTS pedidoInsumo;
+CREATE TABLE IF NOT EXISTS pedidoInsumo (
+	idpedidoInsu INT AUTO_INCREMENT,
+	idinsumo INT NOT NULL,
+	idproveedor INT NOT NULL,
+	idfabrica INT NOT NULL,
+	fecha DATE NOT NULL,
+	precio FLOAT NOT NULL,
+	cantidad INT NOT NULL,
+	PRIMARY KEY (idpedidoInsu),
+	FOREIGN KEY (idproveedor)
+		REFERENCES proveedor (idproveedor),
+	FOREIGN KEY (idinsumo)
+		REFERENCES insumo (idinsumo),
+	FOREIGN KEY (idfabrica)
+		REFERENCES fabrica (idfabrica)
+);
 
-
-
-
-
-
-
-
+DROP TABLE IF EXISTS desarrollo;
+CREATE TABLE IF NOT EXISTS desarrollo (
+	idestacion INT NOT NULL,
+	numChasis VARCHAR(20) NOT NULL,
+	fechaIngreso DATE NOT NULL,
+	fechaSalida DATE NOT NULL,
+	PRIMARY KEY (numChasis, idestacion),
+	FOREIGN KEY (idestacion)
+		REFERENCES estacion (idestacion),
+	FOREIGN KEY (numChasis)
+		REFERENCES vehiculo (numChasis)
+);
 
 
