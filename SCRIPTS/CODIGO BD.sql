32ff805e3730c3fa16ba2e02d04545ef578468d2
fedestf <fede520@live.com>
2019-08-19 18:42:40 -0300

    Update CODIGO BD.sql
    Co-Authored-By: Agustín Gutiérrez <agusguti@users.noreply.github.com>

--- SCRIPTS/CODIGO BD.sql
+++ SCRIPTS/CODIGO BD.sql
@@ -20,7 +20,7 @@ DROP TABLE IF EXISTS fabrica;
 CREATE TABLE IF NOT EXISTS fabrica (
     idfabrica INT NOT NULL,
     nombre VARCHAR(45),
-    PRIMARY KEY (idfabrica)
+    PRIMARY KEY (idfabrica) 
 );
 
 DROP TABLE IF EXISTS modelo;
@@ -38,9 +38,11 @@ CREATE TABLE IF NOT EXISTS lineaDeMontaje (
     idmodelo INT NOT NULL,
     PRIMARY KEY (idlineaDeMontaje),
     FOREIGN KEY (idfabrica)
-        REFERENCES fabrica (idfabrica),
+        REFERENCES fabrica (idfabrica)
+        ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (idmodelo)
         REFERENCES modelo (idmodelo)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS insumo;
@@ -60,13 +62,13 @@ CREATE TABLE IF NOT EXISTS estacion (
     PRIMARY KEY (idestacion),
     FOREIGN KEY (idlineaDeMontaje)
         REFERENCES lineaDeMontaje (idlineaDeMontaje)
-        ON DELETE NO ACTION ON UPDATE NO ACTION,
+        ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (idautoparte)
         REFERENCES autoparte (idautoparte)
-        ON DELETE NO ACTION ON UPDATE NO ACTION,
+        ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (idinsumo)
         REFERENCES insumo (idinsumo)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS concesionaria;
@@ -83,60 +85,69 @@ CREATE TABLE IF NOT EXISTS vehiculo (
     PRIMARY KEY (numChasis),
     FOREIGN KEY (idmodelo)
         REFERENCES modelo (idmodelo)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS pedidoAutopartes;
-CREATE TABLE IF NOT EXISTS pedidoAutopartes(
-	idpedidoAutop INT AUTO_INCREMENT,
-	idautoparte INT NOT NULL,
-	idproveedor INT NOT NULL,
-	idfabrica INT NOT NULL,
-	fecha DATE NOT NULL,
-	precio FLOAT NOT NULL,
-	cantidad INT NOT NULL,
-	PRIMARY KEY (idpedidoAutop),
-	FOREIGN KEY (idautoparte)
-		REFERENCES autoparte (idautoparte),
-	FOREIGN KEY (idproveedor)
-		REFERENCES proveedor (idproveedor),
-	FOREIGN KEY (idfabrica)
-		REFERENCES fabrica(idfabrica)
-	);
+CREATE TABLE IF NOT EXISTS pedidoAutopartes (
+    idpedidoAutop INT AUTO_INCREMENT,
+    idautoparte INT NOT NULL,
+    idproveedor INT NOT NULL,
+    idfabrica INT NOT NULL,
+    fecha DATE NOT NULL,
+    precio FLOAT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY (idpedidoAutop),
+    FOREIGN KEY (idautoparte)
+        REFERENCES autoparte (idautoparte)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idproveedor)
+        REFERENCES proveedor (idproveedor)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idfabrica)
+        REFERENCES fabrica (idfabrica)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 DROP TABLE IF EXISTS venta;
 CREATE TABLE IF NOT EXISTS venta (
-	idventa INT AUTO_INCREMENT,
-	idconcesionaria INT NOT NULL,
-	idmodelo INT NOT NULL,
-	idfabrica INT NOT NULL,
-	cantidad INT NOT NULL,
-	fecha DATE NOT NULL,
-	PRIMARY KEY (idventa),
-	FOREIGN KEY (idconcesionaria)
-		REFERENCES concesionaria (idconcesionaria),
-	FOREIGN KEY (idfabrica)
-		REFERENCES fabrica (idfabrica),
-	FOREIGN KEY (idmodelo)
-		REFERENCES modelo (idmodelo)
+    idventa INT AUTO_INCREMENT,
+    idconcesionaria INT NOT NULL,
+    idmodelo INT NOT NULL,
+    idfabrica INT NOT NULL,
+    cantidad INT NOT NULL,
+    fecha DATE NOT NULL,
+    PRIMARY KEY (idventa),
+    FOREIGN KEY (idconcesionaria)
+        REFERENCES concesionaria (idconcesionaria)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idfabrica)
+        REFERENCES fabrica (idfabrica)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idmodelo)
+        REFERENCES modelo (idmodelo)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS pedidoInsumo;
 CREATE TABLE IF NOT EXISTS pedidoInsumo (
-	idpedidoInsu INT AUTO_INCREMENT,
-	idinsumo INT NOT NULL,
-	idproveedor INT NOT NULL,
-	idfabrica INT NOT NULL,
-	fecha DATE NOT NULL,
-	precio FLOAT NOT NULL,
-	cantidad INT NOT NULL,
-	PRIMARY KEY (idpedidoInsu),
-	FOREIGN KEY (idproveedor)
-		REFERENCES proveedor (idproveedor),
-	FOREIGN KEY (idinsumo)
-		REFERENCES insumo (idinsumo),
-	FOREIGN KEY (idfabrica)
-		REFERENCES fabrica (idfabrica)
+    idpedidoInsu INT AUTO_INCREMENT,
+    idinsumo INT NOT NULL,
+    idproveedor INT NOT NULL,
+    idfabrica INT NOT NULL,
+    fecha DATE NOT NULL,
+    precio FLOAT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY (idpedidoInsu),
+    FOREIGN KEY (idproveedor)
+        REFERENCES proveedor (idproveedor)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idinsumo)
+        REFERENCES insumo (idinsumo)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idfabrica)
+        REFERENCES fabrica (idfabrica)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS desarrollo;
@@ -148,10 +159,10 @@ CREATE TABLE IF NOT EXISTS desarrollo (
     PRIMARY KEY (numChasis , idestacion),
     FOREIGN KEY (idestacion)
         REFERENCES estacion (idestacion)
-        ON DELETE NO ACTION ON UPDATE NO ACTION,
+        ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (numChasis)
         REFERENCES vehiculo (numChasis)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
