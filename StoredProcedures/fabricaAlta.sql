ad517f03e4e9d49d5a422a8db4afad20104b9aac
fedestf <fede520@live.com>
2019-08-19 20:34:46 -0300

    update
    Co-Authored-By: Agustín Gutiérrez <agusguti@users.noreply.github.com>

--- StoredProcedures/fabricaAlta.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `fabricaAlta`(
-in _idfabrica int,
-in _nombre varchar(45)
-)
-BEGIN
- insert into fabrica (idfabrica,nombre) values(_idfabrica,_nombre);
-END
