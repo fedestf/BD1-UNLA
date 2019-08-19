6bb91090ace24d1ff6072c2d80957c3f2efe1ca8
fedestf <fede520@live.com>
2019-08-19 17:31:48 -0300

    Update CARGA DE DATOS.sql
    finalizada
    
    Co-Authored-By: Agustín Gutiérrez <agusguti@users.noreply.github.com>

--- SCRIPTS/CARGA DE DATOS.sql
+++ SCRIPTS/CARGA DE DATOS.sql
@@ -36,7 +36,8 @@ insert into insumo values
 insert into estacion values 
 -- id estacion,id linea montaje,id insumo,id autoparte, tarea
 (1,1,2,null,'carga de combustible y pruebas'),
-(2,2,null,1,'ensamble de chapa');
+(2,2,null,1,'ensamble de chapa'),
+(3,3,3,null,'pintura');
 
 insert into concesionaria values
 -- id, nombre
@@ -52,31 +53,32 @@ insert into vehiculo values
 
 insert into pedidoAutopartes values 
 -- AI,id autoparte,idproveedor,idfabrica,fecha (YYYY-MM-DD),precio,cantidad
-(null,1,1,1,2019/08/21,1500.95,5),
-(null,2,2,1,2019/09/21,1500.95,2),
-(null,3,1,1,2019/07/21,1500.95,1);
+(null,1,1,1,'2019-08-21',1500.95,5),
+(null,2,2,1,'2019-09-21',1500.95,2),
+(null,3,1,1,'2019-07-21',1500.95,1);
 
 insert into venta values
 -- AI,idconcesionaria,idmodelo,idfabrica,cantidad,fecha
-(null,1,1,1,1,2019-12-12),
-(null,2,2,1,1,2019-12-15),
-(null,3,3,1,1,2019-12-18);
+(null,1,1,1,1,'2019-12-12'),
+(null,2,2,1,1,'2019-12-15'),
+(null,3,3,1,1,'2019-12-18');
 
 insert into pedidoInsumo values 
 -- AI,idinsumo,idproveedor,idfabrica,fecha,precio,cantidad
-(null,1,1,1,2019-12-25,1200,1),
-(null,2,2,1,2019-12-27,1300,2),
-(null,3,1,1,2019-12-28,1200,5);
+(null,1,1,1,'2019-12-25',1200,1),
+(null,2,2,1,'2019-12-27',1300,2),
+(null,3,1,1,'2019-12-28',1200,5);
 
 insert into desarrollo values
 -- idestacion, numChasis,fechaIngreso,FechaSalida
-(1,'M1-01',2019-05-10,2019-05-14),
-(2,'M1-01',2019-05-15,2019-05-19),
-(3,'M1-01',2019-05-20,2019-05-24),
-(1,'M2-01',2019-05-10,2019-05-14),
-(2,'M2-01',2019-05-15,2019-05-19),
-(3,'M2-01',2019-05-20,2019-05-24),
-(1,'M3-01',2019-05-10,2019-05-14),
-(2,'M3-01',2019-05-15,2019-05-19),
-(3,'M3-01',2019-05-20,2019-05-24);
+(1,"M1-01",'2019-05-10','2019-05-14'),
+(1,"M2-01",'2019-05-10','2019-05-14'),
+(1,"M3-01",'2019-05-10','2019-05-14'),
+(2,"M1-01",'2019-05-15','2019-05-19'),
+(2,"M2-01",'2019-05-15','2019-05-19'),
+(2,"M3-01",'2019-05-15','2019-05-19'),
+(3,"M1-01",'2019-05-20','2019-05-24'),
+(3,"M2-01",'2019-05-20','2019-05-24'),
+(3,"M3-01",'2019-05-20','2019-05-24');
+
 
