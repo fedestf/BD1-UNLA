ae02acabadd79126b0f3081d15814c890008e97e
sol-dev <54159928+sol-dev@users.noreply.github.com>
2019-09-05 19:26:51 -0300

    bd

--- DER-Update/CODIGO BD.sql
+++ DER-Update/CODIGO BD.sql
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS detalleVenta (
 
 
 
-CREATE TABLE IF NOT EXISTS `mydb`.`vehiculo` (
+CREATE TABLE IF NOT EXISTS vehiculo (
   `numChasis` VARCHAR(20) NOT NULL,
   `idmodelo` INT NOT NULL,
   `idventa` INT NOT NULL,
@@ -99,123 +99,83 @@ CREATE TABLE IF NOT EXISTS `mydb`.`vehiculo` (
   `fechaInicio` DATE NOT NULL,
   `fechaFin` DATE NOT NULL,
   PRIMARY KEY (`numChasis`),
-  UNIQUE INDEX `numChasis_UNIQUE` (`numChasis` ASC) VISIBLE,
-  INDEX `fk_vehiculo_detalleVenta1_idx` (`idmodelo` ASC, `idventa` ASC) VISIBLE,
-  INDEX `fk_vehiculo_estacion1_idx` (`idestacion` ASC) VISIBLE,
-  CONSTRAINT `fk_vehiculo_detalleVenta1`
     FOREIGN KEY (`idmodelo` , `idventa`)
-    REFERENCES `mydb`.`detalleVenta` (`idmodelo` , `idventa`)
+    REFERENCES `detalleVenta` (`idmodelo` , `idventa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_vehiculo_estacion1`
     FOREIGN KEY (`idestacion`)
-    REFERENCES `mydb`.`estacion` (`idestacion`)
+    REFERENCES `estacion` (`idestacion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`insumo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`insumo` (
-    `idinsumo` INT NOT NULL,
-    `nombre` VARCHAR(45) NOT NULL,
-    `eliminado` TINYINT(1) NOT NULL,
-    `fechaEliminado` DATE NULL,
-    PRIMARY KEY (`idinsumo`)
-)  ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS insumo (
+  `idinsumo` INT NOT NULL,
+  `nombre` boolean NOT NULL,
+  `eliminado` TINYINT(1) NOT NULL,
+  `fechaEliminado` DATE default NULL,
+  PRIMARY KEY (`idinsumo`))
+ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`pedidoInsumo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`pedidoInsumo` (
+CREATE TABLE IF NOT EXISTS pedidoInsumo (
   `idpedidoInsu` INT NULL AUTO_INCREMENT,
   `idinsumo` INT NOT NULL,
   `fecha` DATE NOT NULL,
   `precio` FLOAT NOT NULL,
   `cantidad` INT NOT NULL,
-  INDEX `fk_proveedor_has_insumo_insumo1_idx` (`idinsumo` ASC) VISIBLE,
   PRIMARY KEY (`idpedidoInsu`),
-  CONSTRAINT `fk_proveedor_has_insumo_insumo1`
     FOREIGN KEY (`idinsumo`)
-    REFERENCES `mydb`.`insumo` (`idinsumo`)
+    REFERENCES `insumo` (`idinsumo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`estacionAuto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`estacionAuto` (
+CREATE TABLE IF NOT EXISTS estacionAuto(
   `idestacion` INT NOT NULL,
   `numChasis` VARCHAR(20) NOT NULL,
   `fechaIngreso` DATE NOT NULL,
   `fechaSalida` DATE NOT NULL,
-  INDEX `fk_estacion_has_vehiculo_vehiculo1_idx` (`numChasis` ASC) VISIBLE,
-  INDEX `fk_estacion_has_vehiculo_estacion1_idx` (`idestacion` ASC) VISIBLE,
-  PRIMARY KEY (`numChasis`, `idestacion`),
-  CONSTRAINT `fk_estacion_has_vehiculo_estacion1`
+    PRIMARY KEY (`numChasis`, `idestacion`),
     FOREIGN KEY (`idestacion`)
-    REFERENCES `mydb`.`estacion` (`idestacion`)
+    REFERENCES `estacion` (`idestacion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_estacion_has_vehiculo_vehiculo1`
-    FOREIGN KEY (`numChasis`)
-    REFERENCES `mydb`.`vehiculo` (`numChasis`)
+     FOREIGN KEY (`numChasis`)
+    REFERENCES `vehiculo` (`numChasis`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `mydb`.`insumoEstacion`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`insumoEstacion` (
+CREATE TABLE IF NOT EXISTS insumoEstacion(
   `idestacion` INT NOT NULL,
   `idinsumo` INT NOT NULL,
   `cantidad` DOUBLE NULL,
   PRIMARY KEY (`idestacion`, `idinsumo`),
-  INDEX `fk_estacion_has_insumo_insumo1_idx` (`idinsumo` ASC) VISIBLE,
-  INDEX `fk_estacion_has_insumo_estacion1_idx` (`idestacion` ASC) VISIBLE,
-  CONSTRAINT `fk_estacion_has_insumo_estacion1`
     FOREIGN KEY (`idestacion`)
-    REFERENCES `mydb`.`estacion` (`idestacion`)
+    REFERENCES`estacion` (`idestacion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_estacion_has_insumo_insumo1`
-    FOREIGN KEY (`idinsumo`)
-    REFERENCES `mydb`.`insumo` (`idinsumo`)
+   FOREIGN KEY (`idinsumo`)
+    REFERENCES `insumo` (`idinsumo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`proveedorInsumo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`proveedorInsumo` (
+CREATE TABLE IF NOT EXISTS proveedorInsumo(
   `idinsumo` INT NOT NULL,
   `idproveedor` INT NOT NULL,
-  `eliminado` TINYINT(1) NOT NULL,
-  `fechaEliminado` DATE NULL,
+  `eliminado` boolean NOT NULL,
+  `fechaEliminado` DATE default NULL,
   PRIMARY KEY (`idinsumo`, `idproveedor`),
-  INDEX `fk_insumo_has_proveedor_proveedor1_idx` (`idproveedor` ASC) VISIBLE,
-  INDEX `fk_insumo_has_proveedor_insumo1_idx` (`idinsumo` ASC) VISIBLE,
-  CONSTRAINT `fk_insumo_has_proveedor_insumo1`
-    FOREIGN KEY (`idinsumo`)
-    REFERENCES `mydb`.`insumo` (`idinsumo`)
+   FOREIGN KEY (`idinsumo`)
+    REFERENCES `insumo` (`idinsumo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_insumo_has_proveedor_proveedor1`
-    FOREIGN KEY (`idproveedor`)
-    REFERENCES `mydb`.`proveedor` (`idproveedor`)
+   FOREIGN KEY (`idproveedor`)
+    REFERENCES `proveedor` (`idproveedor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-
-
 
