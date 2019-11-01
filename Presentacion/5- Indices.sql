a8c91e53957930e20aa355da3d5c743db7133283
fedestf <fede520@live.com>
2019-11-01 10:19:57 -0300

    Update 5- Indices.sql
    ya le agregue nombres y le tire unos indices mas para ver que onda  , si les parece que quedó , mandenló

--- Presentacion/5- Indices.sql
+++ Presentacion/5- Indices.sql
@@ -12,7 +12,7 @@ create index estacionxLineaDeMont on estacion (idestacion,idlineademontaje) USIN
 create index listaAutos on estacionauto (numChasis) USING HASH;
 create index listaInsumos on insumo (nombre) USING HASH;
 create index listaModelos on modelo (nombre) USING HASH;
-create index listaPedidos on idpedidoinsumo (idinsumo,fecha) USING HASH;
+create index listaPedidos on pedidoinsumo (idinsumo,fecha) USING HASH;
 create index listaProveedores on proveedor (nombre) USING HASH;
 create index listaVentas on venta (fecha,idconcesionaria) USING HASH;
 create index detalleVenta on detalleventa (idventa,idmodelo) USING HASH;
