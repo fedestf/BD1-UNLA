f9aeb618a3caff5b0a3d488aec42825c6def4a3e
fedestf <fede520@live.com>
2019-11-01 10:16:02 -0300

    Update 5- Indices.sql
    ya le agregue nombres y le tire unos indices mas para ver que onda  , si les parece que quedó , mandenló

--- Presentacion/5- Indices.sql
+++ Presentacion/5- Indices.sql
@@ -1,11 +1,24 @@
-create index ponelenombre1 on vehiculo (idestacion,numChasis,fechaInicio,fechaFin) USING HASH; -- 14 REPORTE 11
+create index VehiculosTerminados on vehiculo (idestacion,numChasis,fechaInicio,fechaFin) USING HASH; -- 14 REPORTE 11
 
-create index ponelenombre2 on venta (fecha,idconcesionaria) USING HASH; -- POR SI HAY QUE CONSULTAR VENTAS
+create index InsumoxEst on insumoestacion (idestacion,idlineademontaje,idinsumo,cantidad) USING HASH; -- 14 REPORTE 12
 
-create index ponelenombre3 on insumoestacion (idestacion,idlineademontaje,idinsumo,cantidad) USING HASH; -- 14 REPORTE 12
+create index PedidoxInsum on pedidoinsumo (idinsumo,fecha,cantidad) USING HASH; -- tambien puede ser parte para el 14 reporte 12
 
-create index ponelenombre4 on pedidoinsumo (idinsumo,fecha,cantidad) USING HASH; -- tambien puede ser parte para el 14 reporte 12
+create index lineaDeMontxMod on lineademontaje (idlineademontaje,idmodelo) USING HASH; -- 14 REPORTE 13
 
-create index ponelenombre5 on lineademontaje (idlineademontaje,idmodelo) USING HASH; -- 14 REPORTE 13
+create index estacionxLineaDeMont on estacion (idestacion,idlineademontaje) USING HASH; -- POR SI HACE FALTA LA BUSQUEDA DE DATOS 
 
-create index ponelenombre6 on estacion (idestacion,idlineademontaje) USING HASH; -- POR SI HACE FALTA LA BUSQUEDA DE DATOS 
+- -------------------------------------------- de aca para abajo ya es solo para poner cosas 
+create index listaAutos on estacionauto (numChasis) USING HASH;
+create index listaInsumos on insumo (nombre) USING HASH;
+create index listaModelos on modelo (nombre) USING HASH;
+create index listaPedidos on idpedidoinsumo (idinsumo,fecha) USING HASH;
+create index listaProveedores on proveedor (nombre) USING HASH;
+create index listaVentas on venta (fecha,idconcesionaria) USING HASH;
+create index detalleVenta on detalleventa (idventa,idmodelo) USING HASH;
+
+
+
+
+
+
